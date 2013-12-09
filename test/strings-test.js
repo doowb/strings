3878b53bc6058d65dbe27949be69a8eb75a3eb54
Brian Woodward <brian.woodward@gmail.com>
2013-12-09 00:06:37 -0500

    adding a test folder

---
+++ test/strings-test.js
@@ -0,0 +1,19 @@
+/**
+ * Sellside
+ *
+ * Sellside <http://www.sellside.com>
+ * Created and maintained by Jon Schlinkert and Brian Woodward
+ *
+ * Copyright (c) 2013 Sellside.
+ * Licensed under the MIT License (MIT).
+ */
+
+var expect = require('chai').expect
+
+describe('strings', function() {
+
+  it('should load', function(done) {
+    done();
+  });
+  
+});
