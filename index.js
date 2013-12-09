b9ebaf35f99be75ffb8e3d95ebc99db5e099f42d
Brian Woodward <brian.woodward@gmail.com>
2013-12-09 00:02:02 -0500

    adding initial pass at building a simple template with middleware

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Sellside
+ *
+ * Sellside <http://www.sellside.com>
+ * Created and maintained by Jon Schlinkert and Brian Woodward
+ *
+ * Copyright (c) 2013 Sellside.
+ * Licensed under the MIT License (MIT).
+ */
+
+module.exports = require('./lib/strings');
