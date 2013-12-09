aac7cd250f85be875230da3e5633e65f9aeeb251
Brian Woodward <brian.woodward@gmail.com>
2013-12-09 00:24:55 -0500

    making sure the middle is added

--- test/strings-test.js
+++ test/strings-test.js
@@ -11,13 +11,62 @@
 var expect = require('chai').expect
 var strings = require('../');
 var inspect = require('util').inspect;
+var basename = require('path').basename;
+var extname = require('path').extname;
 
 describe('strings', function() {
 
-  it('should save the structure', function() {
-    var expected = '/{{basename}}/index.html';
-    var structure = strings(expected);
-    expect(structure.structure).to.equal(expected);
+  describe('structure', function() {
+
+    it('should save the structure', function() {
+      var expected = '/{{basename}}/index.html';
+      var structure = strings(expected);
+      expect(structure.structure).to.equal(expected);
+    });
+
   });
 
+  describe('middleware', function() {
+
+    it('should add middleware with use', function() {
+      var middleware = function(path) {
+        return function() {
+          return {
+            basename: basename(path)
+          };
+        };
+      };
+
+      var expected = '/{{basename}}/index.html';
+      var structure = strings(expected);
+      structure.use(middleware('path/to/some/file.html'));
+
+      expect(structure.middleware.length).to.equal(1);
+
+    });
+
+    it('should add middleware with exclude', function() {
+      var middleware = function(path) {
+        return function() {
+          return {
+            basename: basename(path),
+            extname: extname(path)
+          };
+        };
+      };
+
+      var exclude = function() {
+        return ['extname'];
+      };
+
+      var expected = '/{{basename}}/index.html';
+      var structure = strings(expected);
+      structure.use(middleware('path/to/some/file.html'));
+      structure.use(exclude);
+
+      expect(structure.middleware.length).to.equal(2);
+
+    });
+
+  });
 });
