54c164393af5b22d476d106b2a6aa9207147ffae
Brian Woodward <brian.woodward@gmail.com>
2014-01-19 22:10:44 -0500

    adding tests for middleware changes

--- test/strings-test.js
+++ test/strings-test.js
@@ -21,79 +21,79 @@ var utils = require('./utils');
 utils.logging = false;
 
 var advices = {
-  before: function() {
+  before: function () {
     utils.log('Called with: ' + Array.prototype.join.call(arguments));
   }
 };
 
 var strings = meld(require('../'), advices);
 var Strings = meld(require('../lib/strings'), advices);
-var getStrings = function(structure, options) {
-  if (utils.logging) {
-    var args = [ Strings ].concat(slice.call(arguments));
-    return utils.create(args);
-  }
-  return strings(structure, options);
-};
+var getStrings = function (structure, options) {
+    if (utils.logging) {
+      var args = [Strings].concat(slice.call(arguments));
+      return utils.create(args);
+    }
+    return strings(structure, options);
+  };
 
-var pathMiddleware = function(path) {
-  return function() {
-    return {
-      basename: basename(path, extname(path)),
-      ext: extname(path)
+var pathMiddleware = function (path) {
+    return function () {
+      return {
+        basename: basename(path, extname(path)),
+        ext: extname(path)
+      };
     };
   };
-};
 
-var excludeMiddleware = function() {
-  return ['ext'];
-};
+var excludeMiddleware = function () {
+    return ['ext'];
+  };
 
 var testStructure = '/:basename/index:ext';
 
 
-describe('strings', function() {
+describe('strings', function () {
 
-  describe('structure', function() {
+  describe('structure', function () {
 
-    it('should save the structure', function() {
+    it('should save the structure', function () {
       var structure = getStrings(testStructure);
       expect(structure.structure).to.equal(testStructure);
     });
 
-    it('should have an empty structure', function() {
+    it('should have an empty structure', function () {
       var structure = getStrings();
       expect(structure.structure).to.equal('');
     });
 
-    it('should have an empty structure when options are passed in', function() {
+    it('should have an empty structure when options are passed in', function () {
       var structure = getStrings({});
       expect(structure.structure).to.equal('');
     });
 
-    it('should save the structure when options are passed in', function() {
+    it('should save the structure when options are passed in', function () {
       var structure = getStrings(testStructure, {});
       expect(structure.structure).to.equal(testStructure);
     });
 
   });
 
-  describe('middleware', function() {
+  describe('middleware', function () {
 
-    it('should add middleware with use', function() {
+    it('should add middleware with use', function () {
       var structure = getStrings(testStructure);
       structure.use(pathMiddleware('path/to/some/file.html'));
       expect(structure.middleware.length).to.equal(1);
     });
 
-    it('should add middleware with exclude', function() {
+    it('should add middleware with exclude', function () {
       var structure = getStrings(testStructure);
       structure.use(pathMiddleware('path/to/some/file.html'));
       structure.exclude(excludeMiddleware);
       expect(structure.middleware.length).to.equal(2);
     });
 
-    it('should build context from middleware', function() {
+    it('should build context from middleware', function () {
       var structure = getStrings(testStructure);
       structure.use(pathMiddleware('path/to/some/file.html'));
       var expected = {
@@ -104,7 +104,7 @@ describe('strings', function() {
       expect(actual).to.eql(expected);
     });
 
-    it('should build context from middleware with exclusions', function() {
+    it('should build context from middleware with exclusions', function () {
       var structure = getStrings(testStructure);
       structure.use(pathMiddleware('path/to/some/file.html'));
       structure.exclude(excludeMiddleware);
@@ -115,11 +115,36 @@ describe('strings', function() {
       expect(actual).to.eql(expected);
     });
 
+    it('should add middleware with use as object', function () {
+      var structure = getStrings(testStructure);
+      structure.use({
+        foo: 'bar'
+      });
+      expect(structure.middleware.length).to.equal(1);
+    });
+
+    it('should add middleware with exclude as object', function () {
+      var structure = getStrings(testStructure);
+      structure.use(pathMiddleware('path/to/some/file.html'));
+      structure.exclude({
+        basename: ''
+      });
+      expect(structure.middleware.length).to.equal(2);
+    });
+
+    it('should add middlware with exclude as array', function () {
+      var structure = getStrings(testStructure);
+      structure.use(pathMiddleware('path/to/some/file.html'));
+      structure.exclude(['basename']);
+      expect(structure.middleware.length).to.equal(2);
+    });
+
+
   });
 
-  describe('run', function() {
+  describe('run', function () {
 
-    it('should build the final string with no exclusions', function() {
+    it('should build the final string with no exclusions', function () {
       var structure = getStrings(testStructure);
       structure.use(pathMiddleware('path/to/some/file.html'));
       var expected = '/file/index.html';
@@ -127,27 +152,27 @@ describe('strings', function() {
       expect(actual).to.eql(expected);
     });
 
-    it('should build the final string with exclusions', function() {
+    it('should build the final string with exclusions', function () {
       var structure = getStrings(testStructure);
       structure.use(pathMiddleware('path/to/some/file.html'));
       structure.exclude(excludeMiddleware);
 
       var expected = '/file/index:ext';
-      var actual = structure.run();      
+      var actual = structure.run();
       expect(actual).to.eql(expected);
     });
 
-    it('should build the final string from a one time setup', function() {
-        var structure = getStrings();
-        structure.use(pathMiddleware('path/to/some/file.html'));
+    it('should build the final string from a one time setup', function () {
+      var structure = getStrings();
+      structure.use(pathMiddleware('path/to/some/file.html'));
 
-        var expected = 'file';
-        var actual = structure.run(':basename');
-        expect(actual).to.eql(expected);
+      var expected = 'file';
+      var actual = structure.run(':basename');
+      expect(actual).to.eql(expected);
 
-        expected = '.html';
-        actual = structure.run(':ext');
-        expect(actual).to.eql(expected);
+      expected = '.html';
+      actual = structure.run(':ext');
+      expect(actual).to.eql(expected);
     });
 
   });
