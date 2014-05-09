0a2af62f8d25da6f395ff2b11a4cc0b25271dbd1
jonschlinkert <dev@sellside.com>
2014-05-09 00:40:23 -0400

    rewrite slugify function so that a function can be passed as a second parameter

--- middleware/slugify.js
+++ middleware/slugify.js
@@ -1,5 +1,8 @@
 var _str = require('underscore.string');
 
-module.exports = function(str, slugify) {
+module.exports = function slugify(str, slugify) {
+  if (typeof slugify === 'function') {
+    return slugify(str);
+  }
   return slugify ? _str.slugify(str) : str;
 };
