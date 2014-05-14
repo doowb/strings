2cb03c71104f08d1cadbd81561e0462c91c5c31a
Brian Woodward <brian.woodward@gmail.com>
2014-05-14 11:27:57 -0400

    making implementation changes and adding initial tests

--- test/strings2.js
+++ test/strings2.js
@@ -6,148 +6,40 @@
  * Licensed under the MIT License (MIT).
  */
 
-var basename = require('path').basename;
-var extname = require('path').extname;
 var expect = require('chai').expect;
-var Strings = require('../strings.js');
+var Strings = require('../lib/strings2.js');
 
 
 describe('strings', function () {
-  describe('using object based replacements', function () {
-    describe('should build the string', function() {
 
-      it('when given a structure, use all replacement groups', function () {
-        var filepath = '/path/to/my/file.html';
-        var strings = new Strings();
-        strings.set('path', {
-          ':basename': basename(filepath, extname(filepath)),
-          ':ext': extname(filepath)
-        });
-        strings.set('custom', {
-          ':foo': 'bar',
-          ':baz': 'bang'
-        });
-        var actual = strings.process(':foo/:baz/:basename/index:ext');
-        expect(actual).to.eql('bar/bang/file/index.html');
+  describe('caches', function () {
+
+    describe('templates', function() {
+
+      var strings;
+      before(function () {
+        strings = new Strings();
       });
 
-      it('when given a named replacement group and a structure', function () {
-        var filepath = '/path/to/my/file.html';
-        var strings = new Strings();
-        strings.set('path', {
-          ':basename': basename(filepath, extname(filepath)),
-          ':ext': extname(filepath)
-        });
-        var actual = strings.process('/:basename/index:ext', 'path');
-        expect(actual).to.eql('/file/index.html');
+      it('when adding a named template, it should be in `_templates`', function () {
+        var name = 'test1';
+        var template = ':foo/:baz/:basename/index:ext';
+        strings.structure(name, template);
+        expect(strings._templates).to.have.property(name);
+        var actual = strings._templates[name];
+        expect(actual).to.eql(template);
       });
 
-      it('when given a named replacement group, a structure, and a context', function() {
-        var strings = new Strings();
-
-        strings.set('path', {
-          ':basename': function () {
-            return basename(this.filepath, extname(this.filepath));
-          },
-          ':ext': function () {
-            return extname(this.filepath);
-          }
-        });
-
-        var actual = strings.process('/:basename/index:ext', 'path', {
-          filepath: '/path/to/my/file.html'
-        });
-        expect(actual).to.eql('/file/index.html');
+      it('when adding a named template, it should be gotten through `structure(name)`', function () {
+        var name = 'test2';
+        var template = ':basename/index:ext';
+        strings.structure(name, template);
+        var actual = strings.structure(name);
+        expect(actual).to.eql(template);
       });
+
     });
+
   });
 
-
-  describe('using array based replacements', function () {
-    describe('should build the string', function() {
-      it('when given a structure, use all replacement groups', function () {
-        var strings = new Strings();
-
-        var filepath = '/path/to/my/file.html';
-        strings.set('path', [
-          { pattern: ':basename', replacement: basename(filepath, extname(filepath)) },
-          { pattern: ':ext', replacement: extname(filepath) }
-        ]);
-        strings.set('custom', {
-          ':foo': 'bar',
-          ':baz': 'bang'
-        });
-
-        var actual = strings.process(':foo/:baz/:basename/index:ext');
-        expect(actual).to.eql('bar/bang/file/index.html');
-      });
-
-      it('when given a named replacement group and a structure', function () {
-        var strings = new Strings();
-
-        var filepath = '/path/to/my/file.html';
-        strings.set('path', [
-          {
-            pattern: ':basename',
-            replacement: basename(filepath, extname(filepath))
-          },
-          {
-            pattern: ':ext',
-            replacement: extname(filepath)
-          }
-        ]);
-        var actual = strings.process('/:basename/index:ext', 'path');
-        expect(actual).to.eql('/file/index.html');
-      });
-
-      it('when given a named replacement group, a structure, and a context', function() {
-
-        var strings = new Strings();
-        strings.set('path', [
-          {
-            pattern: ':basename',
-            replacement: function () {
-              return basename(this.filepath, extname(this.filepath));
-            }
-          },
-          {
-            pattern: ':ext',
-            replacement: function () {
-              return extname(this.filepath);
-            }
-          }
-        ]);
-
-        var structure = '/:basename/index:ext';
-        var expected = '/file/index.html';
-        var actual = strings.process(structure, 'path', {
-          filepath: '/path/to/my/file.html'
-        });
-        expect(actual).to.eql(expected);
-      });
-
-      it('when given a named replacement group, a structure, and a context', function() {
-        var strings = new Strings();
-
-        strings.set('path', [
-          {
-            pattern: ':basename',
-            replacement: function () {
-              return basename(this.filepath, extname(this.filepath));
-            }
-          },
-          {
-            pattern: ':ext',
-            replacement: function () {
-              return extname(this.filepath);
-            }
-          }
-        ]);
-        var actual = strings.process('/:basename/index:ext', 'path', {
-          filepath: '/path/to/my/file.html'
-        });
-        expect(actual).to.eql('/file/index.html');
-      });
-    });
-  });
 });
