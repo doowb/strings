fa7c30df96cc4b837f9059b268d03455c90437eb
Brian Woodward <brian.woodward@gmail.com>
2014-05-15 12:01:13 -0400

    removing slugify

--- middleware/slugify.js
+++
@@ -1,8 +0,0 @@
-var _str = require('underscore.string');
-
-module.exports = function(str, slugify) {
-  if (typeof slugify === 'function') {
-    return slugify(str);
-  }
-  return slugify ? _str.slugify(str) : str;
-};
